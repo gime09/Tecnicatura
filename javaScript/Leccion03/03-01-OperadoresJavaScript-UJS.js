a98edaea0df44ba07d98c8c8e2974d8c5b787fce
Gimena Gauna <gimegauna12345678@gmail.com>
2024-06-09 01:40:28 -0300

    Clase 7: Implementación de nuevas funcionalidades en JavaScript

---
+++ javaScript/Leccion03/03-01-OperadoresJavaScript-UJS.js
@@ -0,0 +1,17 @@
+//Ejercicios para encontrar números pares e impares
+let parInpar = 7;
+if(parInpar % 2 == 0){
+    console.log("Es un número PAR");
+}
+else{
+    console.log("Es un número IMPAR");
+}
+
+//Ejercicio es mayor de edad
+let edad = 20, adulto = 18;
+if (edad >= adulto){
+    console.log("Es una persona adulta");
+}
+else{
+    console.log("Es una persona menor de edad");
+}
