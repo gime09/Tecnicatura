aa7072768896898b9576f9f2559ff040234c27f7
Gimena Gauna <gimegauna12345678@gmail.com>
2024-08-29 19:14:41 -0300

    Clases en Java con break, continue y labels

--- java/Leccion2_2sem/CicloWhile/src/CicloWhile/EjercicioWhile01.java
+++ java/Leccion2_2sem/CicloWhile/src/CicloWhile/EjercicioWhile01.java
@@ -40,6 +40,28 @@ for(var contando = 0 ; contando < 7; contando++ ){
     
 }
         
+
+for(var contando = 0 ; contando < 7; contando++ ){
+            if (contando % 2 == 0){
+            System.out.println("contando = " + contando);
+            break ;
+            
+        }
+
         
 }
+
+for(var contando = 0 ; contando < 7; contando++ ){
+            if (contando % 2 != 0){
+                continue; 
+        }
+        System.out.println("contando = " + contando);
+          
 }
+
+
+
+
+
+}
+}  
