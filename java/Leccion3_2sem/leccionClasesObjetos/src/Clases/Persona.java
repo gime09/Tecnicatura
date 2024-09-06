104782189163dac4a7b1611571e587b6c81066a9
Gimena Gauna <gimegauna12345678@gmail.com>
2024-09-06 18:35:04 -0300

    Comiteamos leccion 3 clases y objetos en Java

--- java/Leccion3_2sem/leccionClasesObjetos/src/Clases/Persona.java
+++ java/Leccion3_2sem/leccionClasesObjetos/src/Clases/Persona.java
@@ -1,13 +1,19 @@
-
+//La CLASE persona es  nuestra plantilla 
 package Clases;
-
 
 public class Persona {
     
-    //Atributos de una Clase(Características)
+    // Atributos de una Clase (Características)
     String nombre; 
     String apellido;
     
- 
-    
+    // Métodos de la clase (Acciones)
+    public void obtenerInformacion(){
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellido: " + apellido);
+    }
 }
+
+
+    
+    
