d1695310b3333215d925df2b440e645e02010596
Gimena Gauna <gimegauna12345678@gmail.com>
2024-09-06 21:21:54 -0300

    Clase 4 de Java (Clases y Objetos Parte 2)

---
+++ java/ Leccion4_2sem/Aritmetica/src/Operaciones/Aritmetica.java
@@ -0,0 +1,19 @@
+
+package Operaciones;
+
+
+public class Aritmetica {
+    
+    //Atributos de la clase
+    int a;
+    int b;
+    
+    //Método
+    public void sumarNumero(){
+        int resultado = a + b ;
+        System.out.println("resultado = " + resultado);
+
+    }
+    
+    
+}
