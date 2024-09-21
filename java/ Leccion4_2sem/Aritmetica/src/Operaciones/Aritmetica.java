67bf1a52e802658b15b6247d731b75780157f548
Gimena Gauna <gimegauna12345678@gmail.com>
2024-09-20 22:32:52 -0300

    Clase 5 y Objetos Parte 3 / Java

--- java/ Leccion4_2sem/Aritmetica/src/Operaciones/Aritmetica.java
+++ java/ Leccion4_2sem/Aritmetica/src/Operaciones/Aritmetica.java
@@ -15,5 +15,22 @@ public class Aritmetica {
 
     }
     
+    public int sumarConRetorno(){
+      // int resultado = a + b; 
+      // return resultado;
+         return a + b; 
+        
+    }
+    
+    public int sumarConArgumentos(int arg1, int arg2){
+       this.a = arg1; //El argumento a se asigna al atributo this.a
+       this.b = arg2;
+        //return a + b ;
+        return this.sumarConRetorno ();
+        
+    }
+    
+        
+    
     
 }
