926d529c4f894299b9fafbf1bf79e23a92cf11fe
Gimena Gauna <gimegauna12345678@gmail.com>
2024-10-25 23:12:31 -0300

    Clase 10 Parte 6 Abstract y Static / Python

--- python/Leccion9_2sem/FiguraGeometrica.py
+++ python/Leccion9_2sem/FiguraGeometrica.py
@@ -1,7 +1,19 @@
-class FiguraGeometrica:
+from abc import ABC, abstractmethod
+# ABC significa: Abstract Base Class, convierte una clase en abstracta
+
+
+class FiguraGeometrica(ABC):
     def __init__(self, ancho, alto):
-        self._ancho = ancho
-        self._alto = alto
+        if self._validar_valores(ancho):
+            self._ancho = ancho
+        else:
+            self.ancho = 0
+            print(f"Valor erroneo para el ancho: {ancho}")
+        if self._validar_valores(alto):
+            self._alto = alto
+        else:
+            self._alto = 0
+            print(f"Valor erroneo para el alto: {alto}")
 
     # Getter para ancho
     @property
@@ -11,10 +23,10 @@ class FiguraGeometrica:
     # Setter para ancho
     @ancho.setter
     def ancho(self, ancho):
-        if ancho > 0:  # Ejemplo de validación
+        if self._validar_valores(ancho):
             self._ancho = ancho
         else:
-            raise ValueError("El ancho debe ser mayor que 0")
+            print(f"valor erroneo ancho: {ancho}")
 
     # Getter para alto
     @property
@@ -24,11 +36,17 @@ class FiguraGeometrica:
     # Setter para alto
     @alto.setter
     def alto(self, alto):
-        if alto > 0:  # Ejemplo de validación
-            self._alto = alto
-        else:
-            raise ValueError("El alto debe ser mayor que 0")
+       if self._validar_valores(alto):
+           self._alto = alto
+       else:
+           print(f"valor erroneo alto: {alto}")
+
+    @abstractmethod
+    def calcular_area(self):
+        pass
 
     def __str__(self):
         return f"FiguraGeometrica [Ancho: {self._ancho}, Alto: {self._alto}]"
 
+    def _validar_valores(self, valor): # Método encapsulado
+        return True if 0 < valor < 10 else False
--- python/Leccion9_2sem/test_figura_geometrica.py
+++ python/Leccion9_2sem/test_figura_geometrica.py
@@ -1,17 +1,24 @@
 from Cuadrado import Cuadrado
+from FiguraGeometrica import FiguraGeometrica
 from Rectangulo import Rectangulo
 
+print("Creacion de objeto clase Cuadrado".center(50,"-"))
 cuadrado1 = Cuadrado(5, "Azul")
-print(cuadrado1.ancho)
-print(cuadrado1.alto)
+cuadrado1.alto = 7
+cuadrado1.ancho = 7
+# print(cuadrado1.ancho)
+# print(cuadrado1.alto)
 print(f"Cálculo del área del cuadrado: {cuadrado1.calcular_area()}")
 
 # MRO = Method Resolution Order
-print(Cuadrado.mro())
+# print(Cuadrado.mro())
 
 print(cuadrado1)
-
+print("Creación de objeto clase Rectangulo".center(50,"-"))
 rectangulo1 = Rectangulo(3, 8, "verde")
+rectangulo1.ancho = 8
 print(f"Calculo del area del rectangulo: {rectangulo1.calcular_area()}")
 print(rectangulo1)
 
+# figura1 = FiguraGeometrica() No se puede instanciar, es abstracta
+print(Cuadrado.mro())
