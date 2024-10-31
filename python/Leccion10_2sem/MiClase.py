fcbc31cb754d1e152b1fd79a403460c21c8bf8cc
Gimena Gauna <gimegauna12345678@gmail.com>
2024-10-31 16:55:21 -0300

    Clase 10 POO Parte 7 / Python

--- python/Leccion10_2sem/MiClase.py
+++ python/Leccion10_2sem/MiClase.py
@@ -1,15 +1,48 @@
 class MiClase:
- # Variable de clase, este atributo dará a cada objeto el mismo valor
+    # Variable de clase
     variable_clase = "Esta es una variable de clase"
 
-    def __init__(self, variable_instancia): # La variable de instancia, da diferentes valores
+    def __init__(self, variable_instancia):
+        # Variable de instancia
         self.variable_instancia = variable_instancia
 
-print(MiClase.variable_clase)
-miClase1 = MiClase("Esta es una variable de instancia")
+    @staticmethod
+    def metodo_estatico():
+        print(MiClase.variable_clase)
 
-print(miClase1.variable_instancia)
-print(miClase1.variable_clase)
+
+    @classmethod
+    def metodo_clase(cls):
+        print(cls.variable_clase)
+
+    def metodo_instancia(self):
+        self.metodo_clase()
+        self.metodo_estatico()
+        print(self.variable_clase)
+        print(self.variable_instancia)
+
+
+
+
+# Pruebas
+miClase1 = MiClase("Esta es una variable de instancia")
+print(miClase1.variable_instancia)  # Muestra la variable de instancia
+print(miClase1.variable_clase)      # Muestra la variable de clase desde la instancia
+
 miClase2 = MiClase("Esta es otra prueba de variable de instancia")
-print(miClase2.variable_instancia)
-print(miClase2.variable_clase)
+print(miClase2.variable_instancia)  # Muestra la variable de instancia de miClase2
+print(miClase2.variable_clase)      # Muestra la variable de clase desde otra instancia
+
+# Agregar nueva variable de clase
+MiClase.variable_clase2 = "Valor de variable de Clase 2"
+print(MiClase.variable_clase2)      # Muestra la nueva variable de clase
+print(miClase1.variable_clase2)     # Muestra la nueva variable de clase desde la instancia
+print(miClase2.variable_clase2)     # Muestra la nueva variable de clase desde otra instancia
+
+# Llamada al método estático
+MiClase.metodo_estatico()
+
+MiClase.metodo_clase()
+miObjeto1 = MiClase("variable de instancia")
+miObjeto1.metodo_clase()
+miObjeto1.metodo_instancia()
